e4cc2f9bcfba535b0887b023a4dce99cd94b8158
Brett Cunningham <bwcunninghamii@gmail.com>
2018-07-03 11:46:25 -0400

    text on screen upper left

---
+++ udemyrnr/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <Text> Significant Others </Text>
+);
+
+AppRegistry.registerComponent('udemyrnr', () => App);
